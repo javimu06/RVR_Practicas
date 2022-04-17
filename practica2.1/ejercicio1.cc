e99165df73a8b9b888474faeb602b56b222a2d66
javimu06 <javimu06@ucm.es>
2022-04-17 20:35:52 +0200

    Probando salida ejercicio 1

---
+++ practica2.1/ejercicio1.cc
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cerr << "Probando salida\n";
+
+
+
+	return 0;
+}
